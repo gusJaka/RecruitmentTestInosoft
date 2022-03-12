0dbe2aa1d785a1baa260cdc8ad843fa6282a47e2
gusJaka <gjatem@gmail.com>
2022-03-12 12:14:25 +0800

    Make User Interface in ComponentUtama.vue

---
+++ resources/js/components/sub-components/CustomButton.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <button type="btn_type" class="btn" :class="btn_class" @click="handleClick" :style="styles">
+            <i v-if="icon_class != ''" :class="icon_class"></i>
+            <span>
+                {{label}}
+            </span>
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CustomButton',
+    props: {
+        icon_class: {
+            type: String,
+            default: ''
+        },
+        btn_class: {
+            type: String,
+            default: 'btn-primary'
+        },
+        styles: {
+            type: String,
+            default: ''
+        },
+        label: {
+            type: String,
+            default: ''
+        },
+        btn_type: {
+            type:String,
+            default: 'button'
+        }
+    },
+    methods: {
+        handleClick(){
+            this.$emit('btnClick');
+        }
+    }
+}
+</script>
+
+<style scoped>
+span {
+    font-family: Nunito,serif;
+}
+</style>
